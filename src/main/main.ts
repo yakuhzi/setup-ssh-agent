17a3e7128a855254ba8021c1c35ec1b440d8ca7a
yakuhzi <miguel@heidegger-ueberlingen.de>
2019-09-26 23:16:49 +0200

    changed input parameter names

--- src/main/main.ts
+++ src/main/main.ts
@@ -5,8 +5,8 @@ run()
 
 function run(): void {
   try {
-    child_process.execSync(`echo "${process.env.INPUT_SSH_PRIVATE_KEY}" > ~/.ssh/id_rsa`)
-    child_process.execSync(`echo "${process.env.INPUT_SSH_PUBLIC_KEY}" > ~/.ssh/id_rsa.pub`)
+    child_process.execSync(`echo "${process.env.INPUT_PRIVATE}" > ~/.ssh/id_rsa`)
+    child_process.execSync(`echo "${process.env.INPUT_PUBLIC}" > ~/.ssh/id_rsa.pub`)
     child_process.execSync('chmod 600 ~/.ssh/id_rsa')
     child_process.execSync('chmod 600 ~/.ssh/id_rsa.pub')
     child_process.execSync('ssh-add -K ~/.ssh/id_rsa')
