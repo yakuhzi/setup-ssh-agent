a3b99cb4d54d26b5cedd918fb4a3995aebce8111
yakuhzi <miguel@heidegger-ueberlingen.de>
2019-09-26 23:57:20 +0200

    use fs to create ssh files

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,15 +1,24 @@
 import { setFailed } from '@actions/core/lib/core'
 import child_process from 'child_process'
+import fs from 'fs'
 
 run()
 
 function run(): void {
   try {
-    child_process.execSync(`echo "${process.env.INPUT_PRIVATE}" > ~/.ssh/id_rsa`)
-    child_process.execSync(`echo "${process.env.INPUT_PUBLIC}" > ~/.ssh/id_rsa.pub`)
-    child_process.execSync('chmod 600 ~/.ssh/id_rsa')
-    child_process.execSync('chmod 600 ~/.ssh/id_rsa.pub')
-    child_process.execSync('ssh-add -K ~/.ssh/id_rsa')
+    const sshDir = `${process.env.HOME}/Desktop/ssh`
+    const privateFile = `${sshDir}/id_rsa`
+    const publicFile = `${sshDir}/id_rsa.pub`
+
+    fs.mkdirSync(`${sshDir}`, { recursive: true})
+
+    fs.writeFileSync(privateFile, process.env.INPUT_PRIVATE)
+    fs.writeFileSync(publicFile, process.env.INPUT_PUBLIC)
+
+    fs.chmodSync(privateFile, '600')
+
+    child_process.execSync(`ssh-add -K ${sshDir}/id_rsa`)
+    child_process.execSync('eval "$(ssh-agent -s)"')
   } catch (error) {
     console.log(error)
     setFailed(error.message)
