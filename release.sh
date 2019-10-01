0581b3870305e1881284523a1c8aa611ab0d7eda
yakuhzi <miguel@heidegger-ueberlingen.de>
2019-10-01 15:19:25 +0200

    disabled host checking

--- release.sh
+++ release.sh
@@ -9,9 +9,9 @@ npm run tsc
 git add -f node_modules dist
 git commit -m "update node_modules & dist"
 git push -f origin releases/v1
-
 git push origin :refs/tags/v1
 git tag -fa v1 -m "Update v1 tag"
 git push origin v1
 git checkout master
 git branch -D releases/v1
+npm install
--- src/main/main.ts
+++ src/main/main.ts
@@ -12,18 +12,9 @@ function run(): void {
 
     fs.mkdirSync(`${sshDir}`, { recursive: true})
 
-    fs.writeFileSync(`${sshDir}/config`, 'Host Github\n' +
-      '    HostName github.com\n' +
-      '    User git\n' +
-      `    IdentityFile ${sshDir}/id_rsa`
-    )
-
-    fs.writeFileSync(
-      `${sshDir}/known_hosts`,
-      'github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0' +
-        'yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpX' +
-        'LmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEA' +
-        'vGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==\n'
+    fs.writeFileSync(`${sshDir}/config`, 'Host *\n' +
+      '    StrictHostKeyChecking no\n' +
+      '    UserKnownHostsFile=/dev/null\n'
     )
 
     fs.writeFileSync(publicFile, process.env.INPUT_PUBLIC)
