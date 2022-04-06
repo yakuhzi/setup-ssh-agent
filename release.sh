78d09814b73adce89b7c89d934a0a2a267af8043
yakuhzi <miguel@heidegger-ueberlingen.de>
2022-04-06 12:13:20 +0200

    Chore: Update release script

--- release.sh
+++ release.sh
@@ -2,17 +2,17 @@
 # Actions requires a node_modules dir https://github.com/actions/toolkit/blob/master/docs/javascript-action.md#publish-a-releasesv1-action
 # It is recommended not to check these in https://github.com/actions/toolkit/blob/master/docs/action-versioning.md#recommendations
 
-git checkout -b releases/v1
+git checkout -b releases/v2
 rm -rf node_modules dist
 npm install
 npm run build
 git add -f node_modules dist
 git commit -m "Update node_modules & dist"
-git push -f origin releases/v1
+git push -f origin releases/v2
 
-git push origin :refs/tags/v1
-git tag -fa v1 -m "Update v1 tag"
-git push origin v1
+git push origin :refs/tags/v2
+git tag -fa v2 -m "Update v2 tag"
+git push origin v2
 git checkout master
-git branch -D releases/v1
+git branch -D releases/v2
 npm install
